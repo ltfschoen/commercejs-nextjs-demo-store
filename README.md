7ed83c90b7fe1df72fe9325616081ed3ac052b48
Luke Schoen <ltfschoen@gmail.com>
2020-08-30 18:32:57 +1000

    fix: Delete all categories and products now works using id

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Demo Store with Commerce.js and Next.js 🛍️💳
 
+## Custom Changes
+
+* Updated .env with info from https://dashboard.chec.io/settings/developer
+* Created script to remove all seed data by deleting all products and categories to avoid conflicts when modifying the template (i.e. at https://dashboard.chec.io/products and https://dashboard.chec.io/categories)
+
+## Intro
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/157bb2e2-611e-4bbd-9a59-c876f8c3c58a/deploy-status)](https://app.netlify.com/sites/commercejs-demo-store/deploys)
 
 A high-fidelity fully-fledged eCommerce demo store built using the Commerce.js SDK and Next.js with live deployment to Netlify.
--- scripts/commerceJsApi.js
+++ scripts/commerceJsApi.js
@@ -56,19 +56,36 @@ async function fetchCategories() {
 }
 
 /**
- * Delete all categories
- *
- * FIXME - the following does not work, returns error 405
- * The DELETE method is not supported for this route.
- * Supported methods: GET, HEAD, POST.
+ * Fetch products
  */
-async function deleteAllCategories(type) {
+async function fetchProducts() {
+  const url = new URL(
+    "https://api.chec.io/v1/products"
+  );
+
+  let headers = {
+    "X-Authorization": `${process.env.CHEC_SECRET_KEY}`,
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+  };
+
+  return await fetch(url, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(resp => resp.json())
+    .then(json => {
+      return json;
+    });
+}
+
+/**
+ * Delete all categories
+ */
+async function deleteAllCategories() {
   const baseUrl = new URL(
     "https://api.chec.io/v1/categories"
   );
-  let params = {
-    "type": type, // 'slug' or 'id'
-  };
 
   let headers = {
     "X-Authorization": `${process.env.CHEC_SECRET_KEY}`,
@@ -82,12 +99,7 @@ async function deleteAllCategories(type) {
   const categories = await fetchCategories();
   for (const [key, value] of Object.entries(categories.data)) {
     // console.log(`${key}: ${JSON.stringify(value, null, 2)}`);
-    categoryUrl = (type == 'slug')
-      ? new URL(`${baseUrl}/${value.slug}`)
-      : new URL(`${baseUrl}/${value.id}`);
-
-    Object.keys(params)
-      .forEach(key => categoryUrl.searchParams.append(key, params[key]));
+    categoryUrl = new URL(`${baseUrl}/${value.id}`);
 
     category = await fetch(categoryUrl, {
       method: "DELETE",
@@ -105,18 +117,59 @@ async function deleteAllCategories(type) {
 }
 
 /**
+ * Delete all products
+ */
+async function deleteAllProducts() {
+  const baseUrl = new URL(
+    "https://api.chec.io/v1/products"
+  );
+
+  let headers = {
+    "X-Authorization": `${process.env.CHEC_SECRET_KEY}`,
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+  };
+
+  let productUrl;
+  let product;
+  let deletedProducts = [];
+  const products = await fetchProducts();
+  for (const [key, value] of Object.entries(products.data)) {
+    // console.log(`${key}: ${JSON.stringify(value, null, 2)}`);
+    productUrl = new URL(`${baseUrl}/${value.id}`);
+
+    product = await fetch(productUrl, {
+      method: "DELETE",
+      headers: headers,
+    })
+      .then(resp => resp.json())
+      .then(json => {
+        console.log('json: ', json);
+        return json;
+      });
+
+      deletedProducts.push(product);
+  }
+  console.log('Deleted products: ', deletedProducts);
+}
+
+/**
  * Commerce.s API docs https://commercejs.com/docs/api/
  */
 async function main() {
-  // Replace `slug` with the value of a slug shown here https://dashboard.chec.io/categories
-  const slug = 'BODY-PRODUCTS';
-  const category = await fetchCategory(slug);
-  console.log('category: ', category);
+  // // Replace `slug` with the value of a slug shown here https://dashboard.chec.io/categories
+  // const slug = 'BODY-PRODUCTS';
+  // const category = await fetchCategory(slug);
+  // console.log('category: ', category);
 
-  const categories = await fetchCategories();
-  console.log('categories: ', categories);
+  // const categories = await fetchCategories();
+  // console.log('categories: ', categories);
 
-  await deleteAllCategories('slug');
+  // const products = await fetchProducts();
+  // console.log('products: ', products);
+
+  await deleteAllCategories();
+  await deleteAllProducts();
 }
 
 main()
