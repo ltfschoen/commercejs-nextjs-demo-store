d18dabb437b5d0badbabccdc21abb67b4110d26d
Luke Schoen <ltfschoen@gmail.com>
2020-12-13 04:36:45 +1100

    fix mobile menu style more

--- components/common/Header.js
+++ components/common/Header.js
@@ -180,10 +180,17 @@ class Header extends Component {
               style={{
                 ...defaultStyle,
                 ...transitionStyles[state],
+                // Prevent gap being shown at bottom of mobile menu
                 top: '1em'
               }}
             >
-              <div className="position-absolute top-0 left-0 right-0 h-100vh mobile-menu-inner bg-black700 d-flex flex-column justify-content-center">
+              <div
+                className="position-absolute top-0 left-0 right-0 h-100vh mobile-menu-inner bg-black700 d-flex flex-column justify-content-center"
+                style={{
+                  // Prevent mobile menu items (e.g. Home) being hidden behind navbar on small screen heights (e.g. iPhone4 landscape of 320px height)
+                  top: '4em'
+                }}
+              >
                 {mobileMenuLinks.map((item, i) => (
                   <a
                     key={i}
